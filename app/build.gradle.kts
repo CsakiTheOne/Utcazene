0c41fb60c4bd6c76007b2f8a1229e293d72dc408
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2025-07-17 08:44:39 +0200

    2025.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.csakitheone.streetmusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "2025.3"
+        versionCode = 43
+        versionName = "2025.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

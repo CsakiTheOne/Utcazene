6091849f6e30c6315419a3229fbd03209f49ac06
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-10 21:57:34 +0200

    Update EventDetailScreen.kt

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/EventDetailScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/EventDetailScreen.kt
@@ -9,35 +9,39 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil3.compose.AsyncImage
@@ -45,17 +49,19 @@ import com.csakitheone.streetmusic.R
 import com.csakitheone.streetmusic.data.LocalRepository
 import com.csakitheone.streetmusic.navigation.Destination
 import com.csakitheone.streetmusic.navigation.LocalNavBackStack
+import com.csakitheone.streetmusic.navigation.LocalSharedTransitionContext
 import com.csakitheone.streetmusic.ui.components.EventCard
 import com.csakitheone.streetmusic.ui.components.FavoritesIndicator
 import com.csakitheone.streetmusic.ui.components.YouTubeEmbed
 import kotlinx.coroutines.flow.flowOf
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EventDetailScreen(eventId: Int) {
-    val context = LocalContext.current
+    val sharedTransition = LocalSharedTransitionContext.current
     val repository = LocalRepository.current
     val backStack = LocalNavBackStack.current
     val event by repository.getEvent(eventId).collectAsState(initial = null)
@@ -65,121 +71,139 @@ fun EventDetailScreen(eventId: Int) {
         emptyList()
     )).collectAsState(initial = emptyList())
 
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = { Text(event?.artistName ?: "Event Details") },
-                navigationIcon = {
-                    IconButton(onClick = { backStack.removeLastOrNull() }) {
-                        Icon(painterResource(R.drawable.ic_arrow_back), contentDescription = "Back")
-                    }
-                },
-                actions = {
-                    event?.let {
-                        val favSlug = "${it.artistSlug} at ${it.startTime}"
-                        FavoritesIndicator(slug = favSlug)
+            Box {
+                if (repository.shouldShowImage() && !artist?.image.isNullOrBlank()) {
+                    with(sharedTransition.sharedTransitionScope) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .matchParentSize()
+                                .alpha(1f - scrollBehavior.state.collapsedFraction)
+                                .sharedElement(
+                                    sharedTransition.sharedTransitionScope.rememberSharedContentState(
+                                        "image-${artist?.slug}"
+                                    ),
+                                    sharedTransition.animatedVisibilityScope,
+                                ),
+                            model = artist?.image,
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                        )
                     }
                 }
-            )
+                LargeTopAppBar(
+                    title = {
+                        Card(
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.surface.copy(alpha = .75f),
+                            ),
+                        ) {
+                            SelectionContainer {
+                                Text(
+                                    modifier = Modifier.padding(4.dp),
+                                    text = artist?.name ?: event?.artistName ?: "Event Details",
+                                )
+                            }
+                        }
+                    },
+                    navigationIcon = {
+                        FilledIconButton(
+                            onClick = { backStack.removeLastOrNull() },
+                            colors = IconButtonDefaults.filledIconButtonColors(
+                                containerColor = MaterialTheme.colorScheme.surface,
+                                contentColor = MaterialTheme.colorScheme.onSurface,
+                            )
+                        ) {
+                            Icon(
+                                painterResource(R.drawable.ic_arrow_back),
+                                contentDescription = "Back"
+                            )
+                        }
+                    },
+                    actions = {
+                        event?.let {
+                            val favSlug = "${it.artistSlug} at ${it.startTime}"
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surface,
+                                ),
+                            ) {
+                                FavoritesIndicator(slug = favSlug)
+                            }
+                        }
+                    },
+                    scrollBehavior = scrollBehavior,
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                        scrolledContainerColor = MaterialTheme.colorScheme.surface.copy(alpha = .5f),
+                    ),
+                )
+            }
         }
     ) { padding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
                 .verticalScroll(rememberScrollState())
         ) {
-            // Artist Image
-            if (repository.shouldShowImage() && !artist?.image.isNullOrBlank()) {
-                Card(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                ) {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.padding(16.dp),
-                        )
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .heightIn(max = 256.dp)
-                                .clickable {
-                                    artist?.image?.let {
-                                        context.startActivity(
-                                            Intent(
-                                                Intent.ACTION_VIEW,
-                                                it.toUri()
-                                            )
-                                        )
-                                    }
-                                },
-                            model = artist?.image,
-                            contentDescription = null,
-                            contentScale = ContentScale.FillWidth,
-                        )
-                    }
-                }
-                HorizontalDivider()
-            }
-
             Column(
-                modifier = Modifier.padding(16.dp),
+                modifier = Modifier
+                    .padding(
+                        top = padding.calculateTopPadding() + 16.dp,
+                        bottom = padding.calculateBottomPadding() + 16.dp,
+                        start = 16.dp,
+                        end = 16.dp
+                    ),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                // Artist Name and Country
-                Column {
-                    Text(
-                        text = artist?.name ?: event?.artistName ?: "",
-                        style = MaterialTheme.typography.headlineMedium,
-                    )
-                    artist?.country?.let { Text(it) }
-                }
+                // Event Details
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            modifier = Modifier.padding(end = 16.dp),
+                            painter = painterResource(R.drawable.shortcut_places),
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = event?.place ?: "",
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
 
-                // Event Details Card
-                Card {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                    ) {
+                    event?.let { e ->
+                        val startTime = LocalDateTime.parse(e.startTime)
+                        val endTime = LocalDateTime.parse(e.endTime)
+                        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Icon(
-                                painter = painterResource(R.drawable.shortcut_places),
+                                modifier = Modifier.padding(end = 16.dp),
+                                painter = painterResource(R.drawable.shortcut_events),
                                 contentDescription = null,
                             )
-                            Spacer(modifier = Modifier.width(16.dp))
+                            val dateFormatter = DateTimeFormatter.ofPattern("MMMM d, EEEE")
                             Text(
-                                text = event?.place ?: "",
-                                style = MaterialTheme.typography.titleMedium
+                                text = startTime.format(dateFormatter),
+                                style = MaterialTheme.typography.bodyMedium
                             )
                         }
-
-                        event?.let { e ->
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Icon(
-                                    painter = painterResource(R.drawable.shortcut_events),
-                                    contentDescription = null,
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "${startTime.format(timeFormatter)} - ${
+                                endTime.format(
+                                    timeFormatter
                                 )
-                                Spacer(modifier = Modifier.width(16.dp))
-                                val startTime = LocalDateTime.parse(e.startTime)
-                                val endTime = LocalDateTime.parse(e.endTime)
-                                val dateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d.")
-                                val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
-                                Text(
-                                    text = "${startTime.format(dateFormatter)}\n${
-                                        startTime.format(
-                                            timeFormatter
-                                        )
-                                    } - ${endTime.format(timeFormatter)}",
-                                    style = MaterialTheme.typography.bodyMedium
-                                )
-                            }
-                        }
+                            }",
+                            style = MaterialTheme.typography.headlineLarge,
+                            textAlign = TextAlign.Center,
+                        )
                     }
                 }
 
@@ -208,6 +232,12 @@ fun EventDetailScreen(eventId: Int) {
                         style = MaterialTheme.typography.titleLarge
                     )
                     otherTimes.forEach { otherEvent ->
+                        val date = LocalDate.parse(otherEvent.startTime.substringBefore("T"))
+                        val dateFormatter = DateTimeFormatter.ofPattern("MMMM d, EEEE")
+                        Text(
+                            text = date.format(dateFormatter),
+                            style = MaterialTheme.typography.titleMedium
+                        )
                         EventCard(event = otherEvent)
                     }
                 }

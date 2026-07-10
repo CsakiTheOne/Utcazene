3a11d5a41a6f4110a79ad39ce7ef11cd4627073b
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-10 16:28:34 +0200

    Implemented map using iframe

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
@@ -1,15 +1,105 @@
 package com.csakitheone.streetmusic.ui.screens
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.viewinterop.AndroidView
+import com.csakitheone.streetmusic.R
+import com.csakitheone.streetmusic.navigation.LocalNavBackStack
 
 @Composable
 fun MapScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Map Screen")
+    val backStack = LocalNavBackStack.current
+
+    val mapEmbed = """
+        <html>
+
+        <head>
+            <style>
+                html {
+                    overflow: hidden;
+                }
+                
+                body {
+                    background: #888;
+                }
+
+                iframe {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    border: none;
+                }
+            </style>
+        </head>
+
+        <body style="margin:0;padding:0;">
+            Loading map...
+            <iframe src="https://www.google.com/maps/d/embed?mid=12plW9qjTupsu26_lLGD-lnE4jqUczO4U&ehbc=2E312F" width="100%"
+                height="100%"></iframe>
+        </body>
+
+        </html>
+    """.trimIndent()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text("Map") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { backStack.removeLastOrNull() },
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_arrow_back),
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    WebView(context).apply {
+                        settings.domStorageEnabled = true
+                        settings.javaScriptEnabled = true
+                        webViewClient = WebViewClient()
+                        loadDataWithBaseURL(null, mapEmbed, "text/html", "UTF-8", null)
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                        )
+                    }
+                },
+            )
+        }
     }
 }

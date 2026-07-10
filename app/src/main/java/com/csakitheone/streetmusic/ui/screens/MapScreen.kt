e95c09593d9f5ba5a36be7f5da59d0e491e85df4
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-10 16:42:35 +0200

    Much better map

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
@@ -5,13 +5,19 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,7 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.fromColorLong
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.csakitheone.streetmusic.R
 import com.csakitheone.streetmusic.navigation.LocalNavBackStack
@@ -29,61 +38,18 @@ import com.csakitheone.streetmusic.navigation.LocalNavBackStack
 fun MapScreen() {
     val backStack = LocalNavBackStack.current
 
-    val mapEmbed = """
-        <html>
+    val mapUrl =
+        remember { "https://www.google.com/maps/d/embed?mid=12plW9qjTupsu26_lLGD-lnE4jqUczO4U&ehbc=2E312F" }
 
-        <head>
-            <style>
-                html {
-                    overflow: hidden;
-                }
-                
-                body {
-                    background: #888;
-                }
-
-                iframe {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    border: none;
-                }
-            </style>
-        </head>
-
-        <body style="margin:0;padding:0;">
-            Loading map...
-            <iframe src="https://www.google.com/maps/d/embed?mid=12plW9qjTupsu26_lLGD-lnE4jqUczO4U&ehbc=2E312F" width="100%"
-                height="100%"></iframe>
-        </body>
-
-        </html>
-    """.trimIndent()
-
-    Scaffold(
+    Surface(
         modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(
-                title = { Text("Map") },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { backStack.removeLastOrNull() },
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_arrow_back),
-                            contentDescription = "Back"
-                        )
-                    }
-                },
-            )
-        },
-    ) { paddingValues ->
-        Column(
+        // #2d2f2f
+        color = Color(0xFF2d2f2f)
+    ) {
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues),
+                .systemBarsPadding(),
         ) {
             AndroidView(
                 modifier = Modifier.fillMaxSize(),
@@ -92,7 +58,7 @@ fun MapScreen() {
                         settings.domStorageEnabled = true
                         settings.javaScriptEnabled = true
                         webViewClient = WebViewClient()
-                        loadDataWithBaseURL(null, mapEmbed, "text/html", "UTF-8", null)
+                        loadUrl(mapUrl)
                         layoutParams = ViewGroup.LayoutParams(
                             ViewGroup.LayoutParams.MATCH_PARENT,
                             ViewGroup.LayoutParams.MATCH_PARENT,
@@ -100,6 +66,24 @@ fun MapScreen() {
                     }
                 },
             )
+
+            HorizontalFloatingToolbar(
+                expanded = true,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 16.dp),
+            ) {
+                TextButton(
+                    onClick = { backStack.removeLastOrNull() },
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(end = ButtonDefaults.IconSpacing),
+                        painter = painterResource(R.drawable.ic_arrow_back),
+                        contentDescription = null,
+                    )
+                    Text("Exit map")
+                }
+            }
         }
     }
 }

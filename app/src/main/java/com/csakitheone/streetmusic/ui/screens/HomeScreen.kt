82cd17c063397e3caa152abaa0b49ba9ebb3394e
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-10 16:10:48 +0200

    More color in HomeScreen

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/HomeScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
@@ -347,6 +348,79 @@ fun HomeScreen() {
 
             HomeSectionThisYear(repository)
 
+            Text(text = "Parallel events", style = MaterialTheme.typography.titleLarge)
+
+            Card(
+                onClick = {
+                    backStack.add(Destination.Imu)
+                },
+            ) {
+                ListItem(
+                    leadingContent = {
+                        Image(
+                            modifier = Modifier.size(64.dp),
+                            painter = painterResource(R.drawable.imu),
+                            contentDescription = null,
+                        )
+                    },
+                    content = { Text("Íródeák Művészeti Udvar") },
+                    supportingContent = { Text("Full schedule") },
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        headlineColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                        supportingColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    ),
+                )
+            }
+
+            OutlinedCard(
+                onClick = {
+                    backStack.add(Destination.Gyarkert)
+                },
+            ) {
+                ListItem(
+                    leadingContent = {
+                        Image(
+                            modifier = Modifier.size(64.dp),
+                            painter = painterResource(R.drawable.gyarkert_logo),
+                            contentDescription = null,
+                        )
+                    },
+                    content = { Text("Gyárkert") },
+                    supportingContent = { Text("Pont Ott Parti and more") },
+                    colors = ListItemDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        headlineColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        supportingColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                    ),
+                )
+            }
+
+            OutlinedCard(
+                onClick = {
+                    backStack.add(Destination.UnlockFest)
+                },
+            ) {
+                Box(
+                    contentAlignment = Alignment.BottomStart,
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.unlock_fest_banner),
+                        contentDescription = null,
+                    )
+                    Card(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .alpha(.9f),
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(8.dp),
+                            text = "Unlock Fest Vol. VII - TEREM",
+                        )
+                    }
+                }
+            }
+
             Text(text = "Utcazene socials", style = MaterialTheme.typography.titleLarge)
 
             Row(
@@ -407,69 +481,6 @@ fun HomeScreen() {
                     },
                     label = { Text("Facebook") },
                 )
-            }
-
-            Text(text = "Parallel events", style = MaterialTheme.typography.titleLarge)
-
-            OutlinedCard(
-                onClick = {
-                    backStack.add(Destination.Imu)
-                },
-            ) {
-                ListItem(
-                    leadingContent = {
-                        Image(
-                            modifier = Modifier.size(64.dp),
-                            painter = painterResource(R.drawable.imu),
-                            contentDescription = null,
-                        )
-                    },
-                    content = { Text("Íródeák Művészeti Udvar") },
-                    supportingContent = { Text("Full schedule") },
-                )
-            }
-
-            OutlinedCard(
-                onClick = {
-                    backStack.add(Destination.Gyarkert)
-                },
-            ) {
-                ListItem(
-                    leadingContent = {
-                        Image(
-                            modifier = Modifier.size(64.dp),
-                            painter = painterResource(R.drawable.gyarkert_logo),
-                            contentDescription = null,
-                        )
-                    },
-                    content = { Text("Gyárkert") },
-                    supportingContent = { Text("Pont Ott Parti and more") },
-                )
-            }
-
-            OutlinedCard(
-                onClick = {
-                    backStack.add(Destination.UnlockFest)
-                },
-            ) {
-                Box(
-                    contentAlignment = Alignment.BottomStart,
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.unlock_fest_banner),
-                        contentDescription = null,
-                    )
-                    Card(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .alpha(.9f),
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            text = "Unlock Fest Vol. VII - TEREM",
-                        )
-                    }
-                }
             }
 
             Column {

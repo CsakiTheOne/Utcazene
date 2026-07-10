803f109435d5f939dd43f95a5551e62f46377eff
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-10 18:09:46 +0200

    Revert "Map with no distractions"
    This reverts commit 26eefc2a3e04433d64eb012e18e21ac36d318fb2.

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
@@ -3,12 +3,10 @@ package com.csakitheone.streetmusic.ui.screens
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ButtonDefaults
@@ -16,7 +14,6 @@ import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,29 +33,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.csakitheone.streetmusic.R
 import com.csakitheone.streetmusic.navigation.LocalNavBackStack
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun MapScreen() {
-    val activity = LocalActivity.current
-    val scope = rememberCoroutineScope()
     val backStack = LocalNavBackStack.current
 
     val mapUrl =
         remember { "https://www.google.com/maps/d/embed?mid=12plW9qjTupsu26_lLGD-lnE4jqUczO4U&ehbc=2E312F" }
 
-    var webView by remember { mutableStateOf<WebView?>(null) }
-
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = Color(0xFF2d2f2f),
+        // #2d2f2f
+        color = Color(0xFF2d2f2f)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .navigationBarsPadding(),
+                .systemBarsPadding(),
         ) {
             AndroidView(
                 modifier = Modifier.fillMaxSize(),
@@ -67,25 +57,12 @@ fun MapScreen() {
                     WebView(context).apply {
                         settings.domStorageEnabled = true
                         settings.javaScriptEnabled = true
-                        webViewClient = object : WebViewClient() {
-                            override fun onPageFinished(view: WebView?, url: String?) {
-                                scope.launch {
-                                    delay(1.seconds)
-                                    activity?.runOnUiThread {
-                                        view?.evaluateJavascript(
-                                            "document.getElementsByClassName(\"i4ewOd-pzNkMb-haAclf\")[0].style.display = \"none\";",
-                                            null
-                                        )
-                                    }
-                                }
-                            }
-                        }
+                        webViewClient = WebViewClient()
                         loadUrl(mapUrl)
                         layoutParams = ViewGroup.LayoutParams(
                             ViewGroup.LayoutParams.MATCH_PARENT,
                             ViewGroup.LayoutParams.MATCH_PARENT,
                         )
-                        webView = this
                     }
                 },
             )
@@ -93,8 +70,8 @@ fun MapScreen() {
             HorizontalFloatingToolbar(
                 expanded = true,
                 modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(end = 8.dp, bottom = 16.dp),
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 16.dp),
             ) {
                 TextButton(
                     onClick = { backStack.removeLastOrNull() },
@@ -105,15 +82,6 @@ fun MapScreen() {
                         contentDescription = null,
                     )
                     Text("Exit map")
-                }
-                OutlinedButton(
-                    onClick = {
-                        val js =
-                            "document.getElementsByClassName(\"i4ewOd-pzNkMb-ornU0b-b0t70b-Bz112c\")[0].click();"
-                        webView?.evaluateJavascript(js, null)
-                    },
-                ) {
-                    Text("Legend")
                 }
             }
         }

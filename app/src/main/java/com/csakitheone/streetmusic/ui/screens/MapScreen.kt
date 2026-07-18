3c2f6854571ed8bd3a6a02a285767421f19c9a11
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-18 14:44:34 +0200

    Find friends on map

--- app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/csakitheone/streetmusic/ui/screens/MapScreen.kt
@@ -1,14 +1,18 @@
 package com.csakitheone.streetmusic.ui.screens
 
+import android.content.Intent
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.HorizontalFloatingToolbar
@@ -26,17 +30,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.fromColorLong
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
 import com.csakitheone.streetmusic.R
 import com.csakitheone.streetmusic.navigation.LocalNavBackStack
 import com.csakitheone.streetmusic.navigation.LocalSharedTransitionContext
 
 @Composable
 fun MapScreen() {
+    val context = LocalContext.current
     val sharedTransitionContext = LocalSharedTransitionContext.current
     val backStack = LocalNavBackStack.current
 
@@ -94,6 +102,27 @@ fun MapScreen() {
                         contentDescription = null,
                     )
                     Text("Exit map")
+                }
+                TextButton(
+                    onClick = {
+                        context.startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                "https://www.google.com/android/find/people".toUri()
+                            )
+                        )
+                    },
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .padding(end = ButtonDefaults.IconSpacing)
+                            .size(24.dp)
+                            .clip(CircleShape),
+                        painter = painterResource(R.drawable.find_hub_icon),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                    )
+                    Text("Find friends")
                 }
             }
         }

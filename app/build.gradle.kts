dad393f4eca9a230f1a6e2e604c398ae20bdaf0c
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-07-22 09:49:25 +0200

    2026.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.csakitheone.streetmusic"
         minSdk = 29
         targetSdk = 37
-        versionCode = 60
-        versionName = "2026.16"
+        versionCode = 61
+        versionName = "2026.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

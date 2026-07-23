e079a83d85059977d66542f0cb1c84842ccd9dd6
Csáki <jockahun@gmail.com>
2026-07-23 12:53:00 +0200

    2026.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.csakitheone.streetmusic"
         minSdk = 29
         targetSdk = 37
-        versionCode = 62
-        versionName = "2026.18"
+        versionCode = 63
+        versionName = "2026.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
